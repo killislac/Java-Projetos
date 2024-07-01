1d9a704388441f37798a224d906429e57014eba5
killislac <kamilylacerda1@gmail.com>
2024-07-01 10:49:42 -0300

    Exercicios JAVAOO

--- src/IntroducaoMetodos/dominio/Calculadora.java
+++ src/IntroducaoMetodos/dominio/Calculadora.java
@@ -11,4 +11,19 @@ public class Calculadora {
     public double divideDoisNum (double num1 , double num2){
         return num1/num2;
     }
+    public void somaArray(int[] numeros ){
+        int soma = 0;
+        for(int num: numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+    public void somaVarArgs(int... numeros){
+        int soma = 0;
+        for(int num: numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
 }
+
